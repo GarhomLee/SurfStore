da2e6cd4c6b8bb555c067bd71e63cc6c6f0ccd38
JIAHONG LI <jiahongli@JIAHONGs-MacBook-Pro.local>
2019-12-05 22:09:19 -0800

    timeout duration 1500

--- src/Server/StatusManager.java
+++ src/Server/StatusManager.java
@@ -11,7 +11,7 @@ class StatusManager {
 	// private int HEARTBEAT_DURATION = 2500; // heartbeat duration in milliseconds, will not be random
 	// for gradescope
 	private int TIMEOUT_DURATION = 1500; // timeout duration in milliseconds
-	private int HEARTBEAT_DURATION = 500; // heartbeat duration inmilliseconds, will not be random
+	private int HEARTBEAT_DURATION = 50; // heartbeat duration inmilliseconds, will not be random
 
 	private String currentNode; // "host:port" of current node
 	private Vector<String> serversList; // record all servers' "host:port" info, excluding this server
